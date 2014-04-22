93dd7f707c9610d531ea3c386712ad12a5e28218
amoghavs <amoghavs@gmail.com>
2014-04-22 16:08:18 -0700

    Working first version of AnalyseInstrumentStatic.py

--- AnalyseInstrumentStatic.py
+++ AnalyseInstrumentStatic.py
@@ -2,7 +2,7 @@
 import sys,re,getopt
 
 def usage():
-	print "\n\t AnalyseSiminst.py -i <siminst-file> -s <systemID of cache> \n\t Optional inputs: -l <Number of cache levels:default=3> -H <0: Standard memory 1: Hybrid memory> -o <Output-file> "
+	print "\n\t AnalyseSiminst.py -i <static-file> -s <systemID of cache> \n\t Optional inputs: -l <Number of cache levels:default=3> -H <0: Standard memory 1: Hybrid memory> -o <Output-file> "
 	sys.exit()
 def WhiteSpace(Input):
 	temp=re.sub('^\s*','',Input)
@@ -13,13 +13,13 @@ def WhiteSpace(Input):
 def main(argv):
         SiminstFile=''
         debug=0
-        verbose=False
-        SysID=''
-        NumCacheLevels=''
-        HybridMemory=''
-        OutFileName=''
+	OutFileName=''
+	NumFields=''
+	StatNum=''
+	Operand1Idx=''
+	Operand2Idx=''
         try:
-           opts, args = getopt.getopt(sys.argv[1:],"i:s:l:H:o:h:v",["input","sysid","levels","hybrid","output","help","verbose"])
+           opts, args = getopt.getopt(sys.argv[1:],"i:n:s:a:b:o:h:d:v",["input","numfields","statnum","operand1","operand2","output","help","verbose"])
         except getopt.GetoptError:
                 #print str(err) # will print something like "option -a not recognized"
            usage()
@@ -32,35 +32,35 @@ def main(argv):
            elif opt in ("-i", "--input"):
               SiminstFile=arg
               print "\n\t Input file is "+str(SiminstFile)
-           elif opt in ("-s", "--sysid"):
-              StrSysID=WhiteSpace(arg)
-              SysID=int(StrSysID)
-              print "\n\t Sysid is "+str(SysID)
-           elif opt in ("-l", "--levels"):
-           	StrNumCacheLevels=WhiteSpace(arg)
-           	NumCacheLevels=int(StrNumCacheLevels)
-           	print "\n\t Number of Cache Levels: "+str(NumCacheLevels)
-           elif opt in ("-H", "--hybrid"):
-           	StrHybridMemory=WhiteSpace(arg)
-           	HybridMemory=int(StrHybridMemory)
-           	print "\n\t HybridMemory: "+str(HybridMemory)
+	   elif opt in ("-n","-numfields"):
+		NumFieldsStr=WhiteSpace(arg)
+		NumFields=int(NumFieldsStr)
+		print "\n\t Number of fields is "+str(NumFields)
+	   elif opt in ("-s","-statnum"):
+		StatNumStr=WhiteSpace(arg)
+		StatNum=int(StatNumStr)
+		print "\n\t Number of fields is "+str(StatNum)
+	   elif opt in ("-a","--operand1"):
+		Operand1IdxStr=WhiteSpace(arg)
+		Operand1Idx=int(Operand1IdxStr)
+		print "\n\t Operand 1 is "+str(Operand1Idx)		
+	   elif opt in ("-b","-operand2"):
+		Operand2IdxStr=WhiteSpace(arg)
+		Operand2Idx=int(Operand2IdxStr)
+		print "\n\t Operand 1 is "+str(Operand2Idx)
+	   elif opt in ("-d","-debug"):
+		debugStr=WhiteSpace(arg)
+		debug=int(debugStr)
+		print "\n\t Debug option is "+str(debug)
            elif opt in ("-o","--output"):
            	OutFileName=WhiteSpace(arg)
-           	print "\n\t Out file is "+str(OutFileName)
-           elif opt in ("-v","--verbose"):
-           	verbose=False
+           	print "\n\t Out file is "+str(OutFileName)	   
 	   else:  
 		usage()
 		
 
-	if( (SiminstFile=='') or (SysID=='') ):
+	if( (SiminstFile=='') or (NumFields=='') or (StatNum=='') or (Operand1Idx=='') or (Operand2Idx=='') ):
 		usage()
-	if(NumCacheLevels==''):
-		NumCacheLevels=3
-		print "\n\t Using default number of cache levels: "+str(NumCacheLevels)
-	if(HybridMemory==''):
-		HybridMemory=0
-		print "\n\t Assuming the memory is not hybrid \n" ;
 	if(OutFileName!=''):
 		OutStream=open(OutFileName,'w')
 	else:
@@ -69,44 +69,52 @@ def main(argv):
 	Inp=open(SiminstFile)
 	Input=Inp.readlines()
 	NumBlks=0
-	# If this script is used for more than one task, can use the following nested loop as a method.
-	NumCacheLevels+=HybridMemory
 	InputLen=len(Input)
-	OutStream.write("\n\t Format: <Blk-ID> <Hits> <Misses> <Loads> <Stores> \n");
-	HitsID=2
-	MissID=3
-	LoadID=4
-	StoreID=5
+	NumLinesPerBBStats=10
+	LineNumAdjust=StatNum-NumLinesPerBBStats;
+	print "\n\t Assuming that the format of BB stats is not changed and has "+str(NumLinesPerBBStats)+" lines in it ! "
+	print "\n\t Will extract the "+str(StatNum)+" numbered(1-index) line in BB stats. It should be having "+str(NumFields)+" fields in it and the indices of Op1 and Op2 are assumed to be index-1 \n"
+
+	OutStream.write("\n\t Format: <Blk-ID> Op1 Op2 Add Sub Mult Div \n\n");
+	OutStream.write("\n\t ---------------- Requested details/Working assumption ---------------- \n")
+	OutStream.write("\n\t NumLinesPerBBStats: "+str(NumLinesPerBBStats))
+	OutStream.write("\n\t StatNum: "+str(StatNum))
+	OutStream.write("\n\t NumFields: "+str(NumFields))
+	OutStream.write("\n\t Op1: "+str(Operand1Idx)+" Op1: "+str(Operand2Idx)+"\n\n")
+	
 	for LineNum in range(InputLen):
 		CurrLine=Input[LineNum]
-		BlkLine=re.match('\s*BLK\s*\d+\s*0x(.*)\s*.*',CurrLine)
+		BlkLine=re.match('\s*\+vec.*',CurrLine)
 		if BlkLine:
-			BlockID=re.split('\t',BlkLine.group(1))
-			BlockAddressStartIdx=5
-			BlockAddressEndIdx=6
-			#for BlockStats in BlockID:
-			#print "\n\t Is this the BlockID: "+str(BlockID[0])
-			NumBlks+=1
-			MaxLine=LineNum+1+1 #Just need 1st level info now!
-			for AdjLineNum in range(LineNum+1,MaxLine):
-				CurrLine=Input[AdjLineNum]
-				#print "\n\t Investigating line "+str(CurrLine)
-				CacheStats=re.split('\t',CurrLine)
-				if CacheStats:
-					SysIDIdx=1
-					LocalSysID=WhiteSpace(CacheStats[SysIDIdx])
-					#print "\n\t LocalSysID: "+str(LocalSysID)
-					if( SysID==int(LocalSysID)):
-						#print "\n\t Found SysID "+str(SysID)+" related stats in line "+str(AdjLineNum)
-						if(len(CacheStats)<(SysIDIdx+6)):
-							print "\n\t Error: The CacheStat line is expected to have "+str(SysIDIdx+6)+" fields while the specified cache line only has "+str(len(CacheStats))+" number of fields "
-							sys.exit()
-						if( ( int(CacheStats[SysIDIdx+StoreID]) > 100000) and ( (5.0  * int(CacheStats[SysIDIdx+LoadID]) ) < ( int(CacheStats[SysIDIdx+StoreID]) ) ) ):
-						#if(int(CacheStats[SysIDIdx+StoreID]) > 100000):
-							#OutStream.write("\t "+str(BlockID[0])+"\t"+str(CacheStats[SysIDIdx+HitsID])+"\t"+CacheStats[SysIDIdx+MissID]+"\t"+str(CacheStats[SysIDIdx+LoadID])+"\t"+str(CacheStats[SysIDIdx+StoreID]))
-							#OutStream.write("\t "+str(BlockID[0])+"\t"+" Start: "+str(BlockID[BlockAddressStartIdx])+" End: "+str(BlockID[BlockAddressEndIdx]))
-							OutStream.write(" "+str(BlockID[BlockAddressStartIdx])+"\t "+str(BlockID[BlockAddressEndIdx]))
-	print "\n\t NumBlks: "+str(NumBlks)		
+			BlockID=re.split('\t',BlkLine.group(0))
+			#print "\n\t Found +vec at line number "+str(LineNum)
+			FieldLine=LineNum+LineNumAdjust
+			# Can actually loop over all the details/lines if needed! Can/Should adjust LineNumAdjust for that! 
+			if(FieldLine):
+				ReqFields=re.split('\t',Input[FieldLine])
+				# First and last field is common/non-stat line, except maybe the first line! 
+				if debug: 
+					OutStream.write("\n\t Field line: "+str(Input[FieldLine]))
+				if( (len(ReqFields)-2)== NumFields):				
+					if debug:
+						OutStream.write("\n\t So, I have "+str(len(ReqFields)-2)+" fields, it is equal to "+str(NumFields)+" !!")
+					Op1=int(WhiteSpace(ReqFields[Operand1Idx]))
+					Op2=int(WhiteSpace(ReqFields[Operand2Idx]))
+					Add=Op1+Op2
+					Sub=Op1-Op2
+					Mult=Op1*Op2
+					Div=0
+					if(Op2):
+						Div=Op1/Op2
+					BBID=re.match('.*\#\s*0x(.*)',ReqFields[len(ReqFields)-1])
+					if BBID:
+						OutStream.write("\n\t"+str(BBID.group(1))+"\t"+str(Op1)+"\t"+str(Op2)+"\t"+str(Add)+"\t"+str(Sub)+"\t"+str(Mult)+"\t"+str(Div))
+					else:
+						OutStream.write("\n\tN/A \t"+str(Op1)+"\t"+str(Op2)+"\t"+str(Add)+"\t"+str(Sub)+"\t"+str(Mult)+"\t"+str(Div))
+	
+	OutStream.write("\n\n")
+						
+	
 
 if __name__=="__main__":
 	main(sys.argv[1:])
