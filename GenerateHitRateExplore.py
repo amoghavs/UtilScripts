6c7848029e0ef5a6ac1c48202e5e64421b2d18cc
amoghavs <amoghavs@gmail.com>
2014-11-02 02:11:59 -0800

    GenHitRate: Overhauling previous version. Adding ability to multiply num operands in an exprn

--- GenerateHitRateExplore.py
+++ GenerateHitRateExplore.py
@@ -2,7 +2,8 @@
 import sys,getopt,subprocess,re,math,commands,time,copy,random
 
 def usage():
-	print "\t python GenerateHitRateExplore.py -i/--input <Sample-Input-File> -m/--memorylevel <Memory being used>" #-m/--mod <Index-mod> -r/--ratecontrolbegin <RateControlBegin> -q/--ratecontrolend <RateControlEnd> 
+	print "\t python GenerateHitRateExplore.py -i/--input <Sample-Input-File> -m/--memorylevel <Memory being used> -n/--numoperands <NumOperands in an Exprn>" 
+	#-m/--mod <Index-mod> -r/--ratecontrolbegin <RateControlBegin> -q/--ratecontrolend <RateControlEnd> 
 	print "\t Accepted inputs for memory level: L1,L2,L3,Mem "
 	print "\n"
 	sys.exit()
@@ -12,6 +13,33 @@ def RemoveWhiteSpace(Input):
 	Output=re.sub('\s*$','',temp)
 	
 	return Output
+
+def RemoveBraces(Input):
+	temp=re.sub('^\s*\(*','',Input)
+	Output=re.sub('\)\s*$','',temp)
+	#print "\n\t RemoveBraces--Input: "+str(Input)+" tmp: "+str(temp)+" Output "+str(Output)
+	return Output
+
+def grep(*matches):
+    """Returns a generator function that operates on an iterable:
+        filters items in the iterable that match any of the patterns.
+
+    match: a callable returning a True value if it matches the item
+
+    >>> import re
+    >>> input = ["alpha\n", "beta\n", "gamma\n", "delta\n"]
+    >>> list(grep(re.compile('b').match)(input))
+    ['beta\n']
+    """
+    def _do_grep_wrapper(*matches):
+        def _do_grep(lines):
+            for LineNum,line in enumerate(lines):
+                for match in (matches):
+                    if match(line):
+                        yield (line,LineNum)
+                        break
+        return _do_grep
+    return _do_grep_wrapper(*matches)
 
 def FileOpen(FileName,options=''):
 	if(options==''):
@@ -32,8 +60,9 @@ def FileOpen(FileName,options=''):
 def main(argv):
 	InputFileName=''
 	MemoryLevel=''
+	NumOperands=''
 	try:
-		opts, args = getopt.getopt(sys.argv[1:],"i:m:",["input=","memorylevel="])	
+		opts, args = getopt.getopt(sys.argv[1:],"i:m:n:",["input=","memorylevel=","numoperands="])	
 	except getopt.GetoptError:
 		#print str(err) # will print something like "option -a not recognized"
 		usage()
@@ -45,11 +74,16 @@ def main(argv):
 		elif opt in('-m', "--memorylevel"):
 			MemoryLevel=RemoveWhiteSpace(arg)
 			print "\t Memory level name: "+str(MemoryLevel)
-
+		elif opt in('-n',"--numoperands"):
+			NumOperands=int(RemoveWhiteSpace(arg))
+			print "\t NumOperands : "+str(NumOperands)
 		else:
 			usage()
 	if(InputFileName==''):
 		usage()
+	if(NumOperands==''):
+		NumOperands=1
+		print "\t INFO: Assuming default num-operands: "+str(NumOperands)
 	if(MemoryLevel==''):
 		MemoryLevel='L1'
 		print "\t INFO: Assuming default memory level: "+str(MemoryLevel)
@@ -86,16 +120,18 @@ def main(argv):
 			Ops=RemoveWhiteSpace(BreakdownParams[Idx+OpsIdx])
 			print "\t BreakdownParams[Idx+PANIdx] "+str(BreakdownParams[Idx+PANIdx])
 			print "\t ArraySize: "+str(ArraySize)+" DS "+str(DS)+" Ops "+str(Ops)+" PAN: "+str(PAN) 
-			MemLevelParams=str(CurrParam)+'_Size_'+str(ArraySize)+'_DS_'+str(DS)+'_Ops_'+str(Ops)+'_PAN_'
-			StopIdx=Idx+PANIdx+1
+			MemLevelParams=str(CurrParam)+'_Size_'+str(ArraySize)+'_DS_'+str(DS)+'_'
+			PrefixStopIdx=Idx+DSIdx+1
+			SuffixStartIdx=Idx+PANIdx+1
 			break
 		else:
-			Temp=str(CurrParam)+'_'
-			Prefix+=str(Temp)
+			Prefix+=str(CurrParam)+'_'
 
 	Length=len(BreakdownParams)		
 	Suffix=''
-	for Idx in range(StopIdx,Length-1):	
+	#for Idx in range(0,PrefixStopIdx):
+	#	Prefix+=BreakdownParams[Idx]+'_'
+	for Idx in range(SuffixStartIdx,Length-1):	
 		CurrParam=BreakdownParams[Idx]		
 		Temp=str(CurrParam)+'_'
 		Suffix+=str(Temp)
@@ -104,29 +140,93 @@ def main(argv):
 	print "\t Prefix: "+str(Prefix)+" Suffix "+str(Suffix)
 
 	HitRateControlParam='\#define\ '+str(MemoryLevel)+'HitRateControl '
+	THitRateControlParam=str(MemoryLevel)+'HitRateControl'
 	IterationVarModParam='\#define\ '+str(MemoryLevel)+'ExtractBit '
+	TIterationVarModParam=str(MemoryLevel)+'ExtractBit'
 	for CurrLevelIndexMod in range(Begin['LevelIndexMod'],End['LevelIndexMod']+1):
 		for CurrRateControl in range(Begin['RateControl'],End['RateControl']+1):
 			CurrPAN=(100-int( (100/CurrLevelIndexMod)*(CurrRateControl)) );
-			TempMemLevelParams=str(MemLevelParams)+str(CurrPAN)
-			print "\t CurrPAN: "+str(CurrPAN)+" MemLevelParams "+str(TempMemLevelParams)
-			NewFileName=str(Prefix)+str(TempMemLevelParams)+'_'+str(Suffix)
+			OpsTerm=''
+			if(NumOperands==0):
+				OpsTerm='Ops_0'
+			else:
+				for CurrIdx in range(NumOperands-1):
+					if(CurrIdx>0):
+						OpsTerm+='o'
+					else:
+						OpsTerm+='O'
+				OpsTerm='Ops_'+str(OpsTerm)
+
+			print "\t CurrPAN: "+str(CurrPAN)+" MemLevelParams "+str(MemLevelParams)
+			NewFileName=str(Prefix)+str(MemLevelParams)+str(OpsTerm)+'_PAN_'+str(CurrPAN)+'_'+str(Suffix)
 			CpCmd='cp '+str(InputFileName)+' '+str(NewFileName)
 			print "\t CpCmd: "+str(CpCmd)
 		
 			commands.getoutput(CpCmd)
-		
-			ReplaceHitRateControl="perl -i -pe 's/"+str(HitRateControlParam)+"(.*)*/"+str(HitRateControlParam)+str(CurrRateControl)+"/g' "+str(NewFileName)
-			commands.getoutput(ReplaceHitRateControl)
-			print "\t ReplaceHitRateControl: "+str(ReplaceHitRateControl)
-			
-			ReplaceIterationVarMod="perl -i -pe 's/"+str(IterationVarModParam)+"(.*)*/"+str(IterationVarModParam)+str(CurrLevelIndexMod)+"/g' "+str(NewFileName)
-			print "\t "+str(commands.getoutput(ReplaceIterationVarMod))
-			print "\t ReplaceIterationVarMod: "+str(ReplaceIterationVarMod)
+	
+		        NewFile=FileOpen(NewFileName)	
+			GrepOutput=list(grep(re.compile('\s*\.*RandomVar').match)(NewFile))
+			#print "\t GrepOutput "+str(GrepOutput)
+			for CurrTuple in GrepOutput:
+				CurrLine=NewFile[CurrTuple[1]]
+				SplitLine=re.split('=',CurrLine)
+				if(len(SplitLine)>=2):
+					LHS=SplitLine[0]
+					RHSSplit=re.split('\)',SplitLine[1])
+					for CurrEle in RHSSplit:
+						StreamFound=re.match('.*Stream.*',CurrEle)
+						if(StreamFound):
+							#print "\t group: "+str(StreamFound.group(0))
+							VarStreamVar=re.split('\[',StreamFound.group(0))
+							if(len(VarStreamVar)>=2):
+								TempVarStreamVar=RemoveBraces(RemoveBraces(VarStreamVar[0]))
+								ExtractVarNum=re.match('.*Var(\d+)*.*',TempVarStreamVar)
+								if(ExtractVarNum):
+									#print "\t StreamVar "+str(TempVarStreamVar)+" -- "+str(ExtractVarNum.group(1))
+									VarNum=ExtractVarNum.group(1)
+									ReconstructExprn=LHS+' = ( (int) '
+									for Curr in range(NumOperands):
+										if(Curr>0):
+											ReconstructExprn+=' | '
+										Term='( Var'+str(VarNum)+'_Stream0_Operand0[ RandomVar'+str(VarNum)+' + ( OpDiffVar'+str(VarNum)+' * '+str(Curr)+') ] )'
+										#print "\t Term: "+str(Term)
+										ReconstructExprn+=str(Term)
+									ReconstructExprn+=' ) '
+									NewFile[CurrTuple[1]]=ReconstructExprn
+								#print "\t ReconstructExprn: "+str(ReconstructExprn)
+			#print "\n\t HitRateControlParam: "+str(THitRateControlParam)+"---"
+			FindHitRateControl=list(grep(re.compile('.*'+re.escape(THitRateControlParam)+'.*').match)(NewFile))
+			#print "\t FindHitRateControl: "+str(FindHitRateControl)
+			for CurrEle in FindHitRateControl:
+				IfDef=re.match('.*define.*',CurrEle[0])
+				if(IfDef):
+					ReconstructLine='\t #define '+str(THitRateControlParam)+' '+str(CurrRateControl)+'\n'
+					print "\t 1. ReconstructLine: "+str(ReconstructLine)
+					NewFile[CurrEle[1]]=ReconstructLine
 
-			#sys.exit()
-				
-		
+			#print "\t IterationVarModParam "+str(TIterationVarModParam)
+			FindIterationVarModParam=list(grep(re.compile('.*'+re.escape(TIterationVarModParam)+'.*').match)(NewFile))	
+			#print "\t FindIterationVarModParam: "+str(FindIterationVarModParam)
+			for CurrEle in FindIterationVarModParam:
+				IfDef=re.match('.*define.*',CurrEle[0])
+				if(IfDef):
+					ReconstructLine='\t #define '+str(TIterationVarModParam)+' '+str(CurrLevelIndexMod)+'\n'
+					print "\t 2. ReconstructLine: "+str(ReconstructLine)
+					NewFile[CurrEle[1]]=ReconstructLine
+			FindNumOperandsVar=list(grep(re.compile('\s*int\ NumOperandsVar.*').match)(NewFile))
+			for CurrTuple in FindNumOperandsVar:
+				#print "\t CurrLine: "+str(CurrTuple[0])
+				ExtractLHS=re.split('\=',CurrTuple[0])
+				if(len(ExtractLHS)>=2):
+					ReconstructLine=ExtractLHS[0]+' = '+str(NumOperands)+';\n'
+					print "\t 3. ReconstructLine: "+str(ReconstructLine)
+					NewFile[CurrEle[1]]=ReconstructLine
+			#print "\t FindNumOperands: "+str(FindNumOperandsVar)
+			WriteNewFile=open(NewFileName,'w')
+			print "\t Rewriting file: "+str(NewFileName)
+			for CurrLine in NewFile:
+				WriteNewFile.write(CurrLine)
+			sys.exit()	
 
 if __name__=="__main__":
 	main(sys.argv[1:])
